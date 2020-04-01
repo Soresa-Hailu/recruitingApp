f9a2b37fa7f5335c54fc8f9beadeb5455211a168
Soresa-Hailu <craft.soresa@gmail.com>
2020-04-01 11:35:34 -0400

    commit message from classofmarch

---
+++ Selenium/src/myfirstpush.java
@@ -0,0 +1,11 @@
+
+public class myfirstpush {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.print("this the message from classofmarch");
+	}
+
+}
